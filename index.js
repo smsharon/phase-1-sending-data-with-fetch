bab86c0ca46584ed428f5fcec5e4ed617cab51dc
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-31 05:51:38 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch('http://localhost:3000/users', config)
+      .then(response => response.json())
+      .then(data => {
+
+        const newId = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = `New User ID: ${newId}`;
+        document.body.appendChild(idElement);
+  
+        return data; 
+      })
+      .catch(error => {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
